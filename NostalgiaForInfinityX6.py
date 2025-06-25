b4af4e1ac8745b3c872d1389d8f6136444e4b3bc
pimp <pimp>
2025-06-25 15:33:09 +0200

    use pygithub 2.6.1

--- NostalgiaForInfinityX6.py
+++ NostalgiaForInfinityX6.py
@@ -18,7 +18,7 @@ import warnings
 
 log = logging.getLogger(__name__)
 # log.setLevel(logging.DEBUG)
-# hello world
+# hello world 1
 warnings.simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
 #############################################################################################################
